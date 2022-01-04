1cf3ca53873221b0d9caa2b6cf19aeaf7e583440
lana2402 <l_saakovi@cu.edu.ge>
2022-01-05 03:23:19 +0400

    first task

--- src/test/java/Commands/Final.java
+++ src/test/java/Commands/Final.java
@@ -11,12 +11,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+
 import java.util.List;
+
 import org.testng.annotations.*;
 import io.restassured.RestAssured.*;
 import io.restassured.matcher.RestAssuredMatchers.*;
 import org.hamcrest.Matchers.*;
-
 
 
 import com.sun.xml.bind.v2.model.core.TypeRef;
@@ -54,83 +55,71 @@ import static io.restassured.RestAssured.given;
 
 public class Final {
 
-    @Test
-    public void firstTest() {
+    private ChromeOptions chrome;
+    private WebDriver driver;
+    private JavascriptExecutor js;
+
+    public void init() {
+        WebDriverManager.chromedriver().setup();
+        chrome = new ChromeOptions();
+        driver = new ChromeDriver(chrome);
+        js = (JavascriptExecutor) driver;
+    }
+
+    public void registration() {
         RestAssured.baseURI = "https://bookstore.toolsqa.com/Account/v1/User";
         RequestSpecification request = given().header("Content-type", "application/json");
-
         JSONObject requestParams = new JSONObject();
         requestParams.put("userName", "Virrrr"); // Cast
         requestParams.put("password", "Sin1ggggg23!");
         request.body(requestParams.toJSONString());
         Response response = request.post("https://bookstore.toolsqa.com/Account/v1/User");
-
         ResponseBody body = response.getBody();
         System.out.println(body.jsonPath().toString());
-        Assert.assertEquals(response.statusCode(),201);
         // Deserialize the Response body into RegistrationSuccessResponse
-
     }
-    @Test
-    public void LoginForm() throws InterruptedException {
 
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions chrome = new ChromeOptions();
-        WebDriver driver = new ChromeDriver(chrome);
+    public void login() {
         driver.manage().window().maximize();
         driver.get("https://demoqa.com/login");
         WebElement userName = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div/form/div[2]/div[2]/input"));
         userName.sendKeys("Virrrr");
         WebElement password = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div/form/div[3]/div[2]/input"));
         password.sendKeys("Sin1ggggg23!");
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+
         js.executeScript("window.scrollBy(0,250)", "");
         WebElement login = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div/form/div[4]/div/button"));
         login.click();
-
-//        js.executeScript("window.scrollBy(0,300)", "");
-//       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-////        JavascriptExecutor executor = (JavascriptExecutor)driver;
-////        executor.executeScript("document.body.style.zoom = '0.5'");
-//
-//        Thread.sleep(2000);
-        //WebElement deleteButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div/div[3]/div[2]/button"));
-        //WebElement deleteButton = driver.findElement((By.cssSelector("div#text-center button button")));
-        //deleteButton.click();
-
-// Before you try to switch to the so given alert, he needs to be present.
-
-
-
-
-//        Alert alert = driver.switchTo().alert();
-//        assert Objects.equals(alert.getText(), "User Deleted.");
-//        alert.accept();
-//        Helpers.FindAndScroll($(By.xpath("//button[text()='Delete Account']"))).click();
-//        $("#closeSmallModal-ok").click();
-//
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        var deleteAccountSelectorString = "#app > div > div > div.row > div.col-12.mt-4.col-md-6 >" +
-
-                " div.profile-wrapper > div.mt-2.buttonWrap.row > div.text-center.button > button";
-
-        $(deleteAccountSelectorString)
-
-                .click();
-
-        Thread.sleep(200);
-
-
-
-        var okBtnSelector = "#closeSmallModal-ok";
-
-        $(okBtnSelector).click();
-
     }
 
+    @Test
+    public void task1() throws InterruptedException {
 
+        init();
+        registration();
+        login();
+
+        js.executeScript("window.scrollBy(0,300)", "");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        WebElement deleteButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div/div[3]/div[2]/button"));
+        deleteButton.click();
+        WebElement deleteB = driver.findElement(By.id("closeSmallModal-ok"));
+        deleteB.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 300 /*timeout in seconds*/);
+        if (wait.until(ExpectedConditions.alertIsPresent()) != null) {
+            Alert alert = driver.switchTo().alert();
+            assert Objects.equals(alert.getText(), "User Deleted.");
+            alert.accept();
+        }
+        login();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        String actualMsg = driver.findElement(By.id("name")).getText();
+        String errorMsg = "Invalid username or password";
+        if (actualMsg.contains(errorMsg)) {
+            System.out.println("Test Case Passed");
+        }
+
+    }
 }
